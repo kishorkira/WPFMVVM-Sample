bab6ca3b452d6c931c9238bdb906509b41b44a67
Kishor Kumar <kishorkira@gmail.com>
2019-02-01 12:25:01 +0530

    initial

---
+++ WpfSample/ViewModels/SalesViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfSample.Models;
+
+namespace WpfSample.ViewModels
+{
+    public class SalesViewModel : ViewModel
+    {
+        List<Product> _products;
+        int _quantity;
+        decimal _rate;
+        RelayCommand _saveCommand;
+        RelayCommand _onProductSelectedCommand;
+
+        public SalesViewModel()
+        {
+            _products = new List<Product>()
+            {
+                new Product {Id=1,Name="Mouse",Rate=500 },
+                new Product {Id=2,Name="Keyboard",Rate=999.99m },
+                new Product {Id=3,Name="Monitor",Rate=12458.64m }
+            };
+
+        }
+        public Product SelectedProduct { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                SetProperty(ref _quantity, value);
+                OnPropertyChanged("Total");
+
+            }
+        }
+        public decimal Rate
+        {
+            get { return _rate; }
+            set
+            {
+                SetProperty(ref _rate, value);
+                OnPropertyChanged("Total");
+
+            }
+        }
+        public decimal Total { get { return _rate * _quantity; } private set { } }
+        public List<Product> Products { get { return _products; } }
+
+        public ICommand SaveCommand
+        {
+            get
+            {
+                if (_saveCommand == null)
+                {
+                    _saveCommand = new RelayCommand(param => this.CanSave(), param => this.Save());
+                }
+                return _saveCommand;
+
+
+            }
+        }
+        public ICommand OnProductSelectedCommand
+        {
+            get
+            {
+                if (_onProductSelectedCommand == null)
+                {
+                    _onProductSelectedCommand = new RelayCommand(p => CanUpdateOnProductSelected(p as Product), p=> UpdateOnProductSelected(p as Product));
+                }
+                return _onProductSelectedCommand;
+
+
+            }
+        }             
+
+        private bool CanSave()
+        {
+            return Total >= 0;
+        }
+
+        private void Save()
+        {
+            // reminder :: convert to page service
+            // temp code
+            MessageBox.Show("Save Clicked");
+        }
+        private bool CanUpdateOnProductSelected(Product selcetedProduct)
+        {
+            return selcetedProduct != null;
+        }
+        public void UpdateOnProductSelected(Product selcetedProduct)
+        {
+            Rate = selcetedProduct.Rate;
+            Quantity = 0;
+        }
+
+    }
+}
---
+++ WpfSample/Views/Sales.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfSample.Models;
+using WpfSample.ViewModels;
+
+namespace WpfSample.Views
+{
+    /// <summary>
+    /// Interaction logic for Sales.xaml
+    /// </summary>
+    public partial class Sales : Page
+    {
+        SalesViewModel ViewModel { get { return (DataContext as SalesViewModel); }}
+        public Sales()
+        {
+            InitializeComponent();
+            DataContext = new SalesViewModel();
+        }
+        private void OnProductSelected(object sender, EventArgs e)
+        {
+            var selectedItem = (sender as ComboBox).SelectedItem;
+            if (!ViewModel.OnProductSelectedCommand.CanExecute(ViewModel.SelectedProduct)) return;
+            ViewModel.OnProductSelectedCommand.Execute(ViewModel.SelectedProduct);
+               
+        }
+    }
+}
